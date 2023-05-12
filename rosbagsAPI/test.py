184f820708c37e91b2eb3bb71976064442e172de
Beatriz Fernandes <85555689+beaf24@users.noreply.github.com>
2023-05-12 10:56:53 +0100

    class static_map

--- rosbagsAPI/test.py
+++ rosbagsAPI/test.py
@@ -1,8 +1,10 @@
 import rosbags
 import struct
 import pprint
+import numpy as np
 from rosbags.rosbag1 import Reader
 from rosbags.serde import deserialize_cdr, ros1_to_cdr
+from PIL import Image
 
 class Range:
     def __init__(self, distance, angle):
@@ -27,8 +29,110 @@ class LaserData:
 
 laserData = []
 
+class Static_Map:
+    def __init__(self, grid_size: int =60, resolution:int = 1):
+        self.l0 = np.log(0.5/0.5)
+        self.l_free = 0.2 
+        self.l_occ = 0.8
+        self.grid_size = grid_size # tamanho em metros da grelha
+        self.resolution = resolution # resolução das celulas da grelha
+        self.logodds = self.l0*np.ones((grid_size*resolution, grid_size*resolution)) #grelha de 60 por 60
+
+    def bresenham(self, start, end):
+        """
+        Bresenham's Line Generation Algorithm
+        https://www.youtube.com/watch?v=76gp2IAazV4
+        """
+        # step 1 get end-points of line 
+        (x0, y0) = start
+        (x1, y1) = end
+
+        # step 2 calculate difference
+        dx = abs(x1 - x0)
+        dy = abs(y1 - y0)
+        m = dy/dx
+        
+        # step 3 perform test to check if pk < 0
+        flag = True
+        
+        line_pixel = []
+        line_pixel.append((x0,y0))
+        
+        res = self.grid_size * self.resolution
+
+        step = self.resolution
+        if x0>x1 or y0>y1:
+            step = -1
+
+        mm = False   
+        if m < 1:
+            x0, x1 ,y0 ,y1 = y0, y1, x0, x1
+            dx = abs(x1 - x0)
+            dy = abs(y1 - y0)
+            mm = True
+            
+        p0 = 2*dx - dy
+        x = x0
+        y = y0
+        
+        for i in range(abs(y1-y0)):
+            if flag:
+                x_previous = x0
+                p_previous = p0
+                p = p0
+                flag = False
+            else:
+                x_previous = x
+                p_previous = p
+                
+            if p >= 0:
+                x = x + step
+
+            p = p_previous + 2*dx -2*dy*(abs(x-x_previous))
+            y = y + 1
+            
+            if mm:
+                line_pixel.append((y,x))
+            else:
+                line_pixel.append((x,y))
+                
+        line_pixel = np.array(line_pixel)
+        
+        return line_pixel
+
+    def occupancy_grid_mapping(self, x_t:tuple, z_t:np.array):
+        res = self.grid_size * self.resolution
+        for beam in z_t:  #beam é um tuplo da posição da deteção (distance (m), angle (rads))
+            distance, angle = beam
+            beam_xy = (distance*np.cos(angle), distance*np.sin(angle))
+            
+            line = self.bresenham(start = x_t, end = beam_xy)
+            for mi in line:
+                pos_x, pos_y = mi
+                if mi == line[-1] and z_t != 'NaN': # tem de atingir alvo
+                    self.logodds[pos_x, pos_y] = self.logodds[pos_x, pos_y] + self.l_occ - self.l0
+                else:
+                    self.logodds[pos_x, pos_y] = self.logodds[pos_x, pos_y] + self.l_free - self.l0
+
+    def logodds_to_prob(self):
+        """
+        Converter logodds em probabilidades
+        """
+        res = self.grid_size * self.resolution
+        map_prob = np.zeros(self.logodds.shape)
+        for i in np.arange(res):
+            for j in np.arange(res):
+                map_prob[i, j] = 1 - 1/(1+np.exp(self.logodds[i, j]))
+        
+        return map_prob
+    
+    def get_map(self):
+        prob_map = self.logodds_to_prob
+        im = Image.fromarray(prob_map)
+        im.show()
+
 # create reader instance
-with Reader('parado.bag') as reader:
+with Reader('05_05_Scan&Pose/parado.bag') as reader:
     # topic and msgtype information is available on .connections list
     for connection in reader.connections:
         print(connection.topic, connection.msgtype)
@@ -47,3 +151,8 @@ for data in laserData:
         print("Angle: " + str(range.angle) + " rads   ||||   Distance: " + str(range.distance) + " meters")
         
     break  # break so it doesnt print too much, just prints the first scan
+
+import numpy as np
+
+
+
