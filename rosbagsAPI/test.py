39f3317d953e739466e5896845effb2517faf4e1
Beatriz Fernandes <85555689+beaf24@users.noreply.github.com>
2023-05-13 15:16:40 +0100

    update

--- rosbagsAPI/test.py
+++ rosbagsAPI/test.py
@@ -41,33 +41,69 @@ class Static_Map:
         self.resolution = resolution # resolução das celulas da grelha
         self.logodds = self.l0*np.ones((grid_size*resolution, grid_size*resolution)) #grelha de 60 por 60
 
-    def bresenham(start:tuple, end:tuple):
-        """Bresenham's line algorithm implementation"""
-        x0, y0 = start
-        x1, y1 = end
+    def bresenham(self, start, end):
+        """
+        Bresenham's Line Generation Algorithm
+        https://www.youtube.com/watch?v=76gp2IAazV4
+        """
+        # step 1 get end-points of line 
+        (x0, y0) = start
+        (x1, y1) = end
+
+        print(x0, y0, x1, y1)
+
+        # step 2 calculate difference
         dx = abs(x1 - x0)
         dy = abs(y1 - y0)
-        sx = 1 if x0 < x1 else -1
-        sy = 1 if y0 < y1 else -1
-        err = dx - dy
+        m = dy/dx
         
-        points = []
-        while True:
-            points.append((x0, y0))
+        # step 3 perform test to check if pk < 0
+        flag = True
+        
+        line_pixel = []
+        line_pixel.append((x0,y0))
+        
+        res = self.grid_size * self.resolution
+
+        step = self.resolution
+        if x0>x1 or y0>y1:
+            step = -self.resolution
+
+        mm = False   
+        if m < 1:
+            x0, x1 ,y0 ,y1 = y0, y1, x0, x1
+            dx = abs(x1 - x0)
+            dy = abs(y1 - y0)
+            mm = True
             
-            if x0 == x1 and y0 == y1:
-                break
+        p0 = 2*dx - dy
+        x = x0
+        y = y0
+        
+        for i in range(int(abs(y1-y0))):
+            if flag:
+                x_previous = x0
+                p_previous = p0
+                p = p0
+                flag = False
+            else:
+                x_previous = x
+                p_previous = p
                 
-            e2 = 2 * err
-            if e2 > -dy:
-                err -= dy
-                x0 += sx
+            if p >= 0:
+                x = x + step
+
+            p = p_previous + 2*dx -2*dy*(abs(x-x_previous))
+            y = y + 1
+            
+            if mm:
+                line_pixel.append((y,x))
+            else:
+                line_pixel.append((x,y))
                 
-            if e2 < dx:
-                err += dx
-                y0 += sy
-                
-        return points
+        line_pixel = np.array(line_pixel).astype(int)
+        
+        return line_pixel
 
     def occupancy_grid_mapping(self, x_t:tuple, z_t:np.array):
         res = self.grid_size * self.resolution
