9f59f79ec0f3d4c7972538e08572b25bf1d8ec1b
Beatriz Fernandes <85555689+beaf24@users.noreply.github.com>
2023-05-19 09:24:08 +0100

    Update bresenham.py

--- microsimulator/bresenham.py
+++ microsimulator/bresenham.py
@@ -3,54 +3,66 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-def determine_coords(x0, y0, z_angle, laser_angle, laser_measure, meters_pixel_ratio):
-    line_angle = z_angle + laser_angle
-    
-    x1 =  x0 + (laser_measure * math.cos(line_angle) ) / meters_pixel_ratio
-    y1 =  y0 + (laser_measure * math.sin(line_angle) ) / meters_pixel_ratio
-    
-    return int(x1), int(y1)
+class Bresenham():
+    def determine_coords(x0, y0, z_angle, laser_angle, laser_measure, meters_pixel_ratio):
+        line_angle = z_angle + laser_angle
 
-
-def bres_algo(x0, y0, x1, y1, map):
-    pointX = x0
-    pointY = y0
-
-    dX = abs( x1 - x0 )
-    dY = -abs( y1 - y0 )
-    increment_X = None
-    increment_Y = None
-    error =  dX + dY
-
-    
-    if( x0 < x1 ):
-        increment_X = 1
-    else:
-        increment_X = -1
-
-    if( y0 < y1 ):
-        increment_Y = 1
-    else:
-        increment_Y = -1
-
-    while(True):
-        if ( pointX == x1) and ( pointY == y1):
-            break
+        if( abs(line_angle) > 2*math.pi ):
+            if( line_angle > 0):
+                line_angle = line_angle - 2*math.pi
+            else:
+                line_angle = line_angle + 2*math.pi
+        print(line_angle*180/math.pi)
         
-        map[int(pointX)][int(pointY)] = map[int(pointX)][int(pointY)] + 0.2
-        e2 = error*2
+        x1 =  x0 + (laser_measure * math.cos(line_angle) ) / meters_pixel_ratio
+        y1 =  y0 + (laser_measure * math.sin(line_angle) ) / meters_pixel_ratio
+        print(y1)
 
-        if ( e2 >= dY ):
-            if ( pointX == x1 ):
+        return [ int(x1), int(y1) ]
+
+
+    def bres_algo(x0, y0, x1, y1):
+        pointX = x0
+        pointY = y0
+        bresenham_points = []
+
+        dX = abs( x1 - x0 )
+        dY = -abs( y1 - y0 )
+        increment_X = None
+        increment_Y = None
+        error =  dX + dY
+
+        
+        if( x0 < x1 ):
+            increment_X = 1
+        else:
+            increment_X = -1
+
+        if( y0 < y1 ):
+            increment_Y = 1
+        else:
+            increment_Y = -1
+
+        while(True):
+            bresenham_points.append([pointX, pointY])
+
+            if ( pointX == x1) and ( pointY == y1):
                 break
 
-            error = error + dY
-            pointX = pointX + increment_X
+            e2 = error*2
 
-        if ( e2 <= dX ):
-            if ( pointY == y1):
-                break
-            
-            error = error + dX
-            pointY = pointY + increment_Y
+            if ( e2 >= dY ):
+                if ( pointX == x1 ):
+                    break
 
+                error = error + dY
+                pointX = pointX + increment_X
+
+            if ( e2 <= dX ):
+                if ( pointY == y1):
+                    break
+                
+                error = error + dX
+                pointY = pointY + increment_Y
+
+        return bresenham_points
