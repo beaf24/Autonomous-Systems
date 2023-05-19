96cf046cb0f57917686ba2f4954784272f1e6eb0
joelhenwang <52075169+joelhenwang@users.noreply.github.com>
2023-05-19 07:06:13 +0000

    Update mapping.py

--- microsimulator/mapping.py
+++ microsimulator/mapping.py
@@ -21,16 +21,21 @@ robot_orientation = 0
 
 bag = rosbag.Bag("piso5_4_amcl.bag")
 
-
+i = 0
 for topic, msg, t in bag.read_messages(topics=['/amcl_pose', '/scan']):
     if (topic == "/amcl_pose"):
         relative_pos = msg.pose.pose.position  # in meters, we still need to convert to the equivalent in pixels
         relative_orientation = msg.pose.pose.orientation
-        robot_x = init_x + (relative_pos.x / meter_per_pixel)
-        robot_y = init_y + (relative_pos.y / meter_per_pixel)
+        robot_x = int(init_x + (relative_pos.x / meter_per_pixel))
+        robot_y = int(init_y + (relative_pos.y / meter_per_pixel))
         robot_orientation = z_quaternion_to_euler( w= relative_orientation.w, z= relative_orientation.z)
 
     if(topic == "/scan"):
+        # if i == 1 : 
+        #     i = 0
+        #     continue
+
+        # i = i+1
         laserScan = LaserData( minAngle= msg.angle_min, maxAngle= msg.angle_max, angleIncrement= msg.angle_increment, ranges= msg.ranges)
 
         for measure in laserScan.laserData:  # attribute "laserData" in "laserScan" is a list of laser beams, with each having a distance measured and angle of the measurement
@@ -38,10 +43,7 @@ for topic, msg, t in bag.read_messages(topics=['/amcl_pose', '/scan']):
 
             obstacle_x, obstacle_y = determine_coords( x0= robot_x, y0= robot_y, z_angle= robot_orientation, laser_angle= measure.angle, laser_measure= measure.distance, meters_pixel_ratio= meter_per_pixel)
             
-            free_cells = bres_algo(robot_x, robot_y, obstacle_x, obstacle_y)
-
-            for cell in free_cells:
-                map[cell[0]][cell[1]] = map[cell[0]][cell[1]] + 0.2
+            bres_algo(robot_x, robot_y, obstacle_x, obstacle_y, map)
 
             map[obstacle_x][obstacle_y] = map[obstacle_x][obstacle_y] + 0.8
 
