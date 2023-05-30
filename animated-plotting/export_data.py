a2482d3cbc7a94f7ef45527894296afc291c8ffe
joelhenwang <52075169+joelhenwang@users.noreply.github.com>
2023-05-30 22:55:40 +0100

    updated exporting

--- animated-plotting/export_data.py
+++ animated-plotting/export_data.py
@@ -23,34 +23,39 @@ except:
     print("file does not exist, creating it")
 
 try:
-    os.remove("./export2.txt")
-    os.remove("./size2.txt")
+    os.remove("./export.txt")
+    os.remove("./size.txt")
 except:
     print("file does not exist")
 
 file1 = open("bag_data.txt", "+a")
 
 i = 0
+export_scan = False
 
 for topic, msg, t in bag.read_messages(topics=['/amcl_pose', '/scan']):
     if (topic == "/amcl_pose"):
         relative_pos = msg.pose.pose.position  # in meters, we still need to convert to the equivalent in pixels
         relative_orientation = msg.pose.pose.orientation
+        export_scan = True
 
     
-    if(topic == "/scan"):
+    if(topic == "/scan" and export_scan):
         inc = 0
-        i = i+1
-
+        
+        if i == 1:
+            i = 0
+            export_scan = False
+            continue
         for measure in msg.ranges:
-            if( str(measure) == "nan" or measure > msg.range_max or measure < msg.range_min): 
+            if( str(measure) == "nan" or measure > msg.range_max - 0.1 or measure < msg.range_min): 
                 inc = inc + 1
                 continue
-            print(msg.range_max)
             file1.write( str( f'{relative_pos.x:.10f}' ) + " " + str( f'{relative_pos.y:.10f}' ) + " " + str(relative_orientation.w) + " " + str(relative_orientation.z) + " ")
             file1.write( str( msg.angle_min + (inc * msg.angle_increment) ) + " " + str(measure) + "\n")
             inc = inc + 1
-    
+
+        i = i+1
             
 
 file1.close()
