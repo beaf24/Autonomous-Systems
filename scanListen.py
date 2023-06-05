1d2593bd41d3a8dc048b84cfebb14eef43656cf1
Diogo Neves <neves.diogo98@gmail.com>
2023-06-05 14:40:14 +0100

    tweeks to scan listener

--- scanListen.py
+++ scanListen.py
@@ -1,18 +1,18 @@
 import rospy
 from sensor_msgs.msg import LaserScan
+import os
 
 class Listener():
     def __init__(self):
         rospy.init_node('scan_listener', anonymous=False)
-        rospy.loginfo("Start!")
         rospy.Subscriber('/scan', LaserScan, self.callback)
-        self.ranges = []
+        self.msg = []
 
-    def callback(self, msg):
-        self.ranges = msg.ranges
+    def callback(self, msgread):
+        self.msg = msgread
 
 
 if __name__ == '__main__':
     listen = Listener()
-    while not rospy.is_shutdown():
-        continue
+    while rospy.wait_for_message("/scan", LaserScan, timeout=10):
+        rospy.loginfo(listen.msg)
